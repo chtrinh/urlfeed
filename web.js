93f5a0e66af080e3384eb37ccfe326e807b932b6
def <defsan@gmail.com>
2012-06-18 10:43:10 -0700

    Adding request

--- web.js
+++ web.js
@@ -3,6 +3,19 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var request = require('request');
+  request(
+    { method: 'GET'
+    , uri: 'http://google.com'
+    }
+  , function (error, response, body) {
+      if(response.statusCode == 200){
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  )
+
   response.send('Hello World!');
 });
 
