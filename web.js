c747d299967863c2c61f4b0cafe9d993994551c4
Maximiliano <max@youthwar.com>
2012-06-21 23:07:08 -0400

    no framework

--- web.js
+++ web.js
@@ -1,46 +1,16 @@
-var express = require('express');
+  var http = require('http')
+  ,   fs = require('fs');
 
-var app = express.createServer(express.logger());
+var router = function(req,res){
 
-app.post('/process', function(request, response) {
-  var request = require('request');
-  request(
-    { method: 'GET'
-    , uri: 'http://google.com'
-    }
-  , function (error, response, body) {
-      if(response.statusCode == 200){
-        console.log('error: '+ response.statusCode)
-        console.log(body)
-      }
-    }
-  )
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World\n');
 
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.write(JSON.stringify(yourObject));
-  response.close();
-});
+};
 
-app.get('/', function(request, response){
-  
-  var sys = require('sys'),
-      fs = require('fs'),
-      index;
-   
-  fs.readFile('./index.html', function (err, data) {
-      if (err) {
-          throw err;
-      }
-      index = data;
-  });
+http.createServer(function (req, res) {
+  router(req,res);
+}).listen(5000, '127.0.0.1');
 
-  response.writeHeader(200, {"Content-Type": "text/html"});
-  response.write(index);
-  response.close();
 
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log('running at http://127.0.0.1:5000/');
