0cfd426f3a25b8ddb849308f11e4c76b80293a47
Maximiliano <max@youthwar.com>
2012-06-21 23:23:00 -0400

    simple router

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
   var http = require('http')
+  ,   url = require('url')
   ,   fs = require('fs');
 
 var router = function(req,res){
@@ -7,8 +8,20 @@ var router = function(req,res){
   res.end('Hello World\n');
 
 };
+var renderErPage = function(req,res){
+  res.writeHead(400);
+  res.end('fuck you no path');
+};
+
 
 http.createServer(function (req, res) {
+  var newProc = new RegExp('^/?$');
+  var pathname = url.parse(req.url).pathname;
+  if(newProc.test(pathname)){
+    router(req,res);
+  }else{
+  renderErPage(req,res);
+  }
   router(req,res);
 }).listen(5000, '127.0.0.1');
 
